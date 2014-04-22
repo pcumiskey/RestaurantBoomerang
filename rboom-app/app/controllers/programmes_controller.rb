3381d32973622ba0077f705c0ffd151b79dc56f2
Patrick Cumiskey <pcumiskey@gmail.com>
2014-04-22 22:29:07 +0100

    programme linker

--- rboom-app/app/controllers/programmes_controller.rb
+++ rboom-app/app/controllers/programmes_controller.rb
@@ -4,4 +4,10 @@ def index
 	    @boomerang = Boomerang.find(params[:boomerang_id])
         @programmes = @boomerang.programmes
 	end
+
+	def show
+		@boomerang = Boomerang.find(params[:boomerang_id])
+		@programme = Programme.find(params[:id])
+	end
+
 end
