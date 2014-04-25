cd993f98a025f2c91d2bd83c1cf348ea4cbfff0b
Patrick Cumiskey <pcumiskey@gmail.com>
2014-04-25 23:58:43 +0100

    create messages

--- rboom-app/app/controllers/messages_controller.rb
+++ rboom-app/app/controllers/messages_controller.rb
@@ -1,2 +1,35 @@
 class MessagesController < ApplicationController
+
+	def index
+		 @boomerang = Boomerang.find(params[:boomerang_id])
+         @programme = Programme.find(params[:programme_id])
+         @messages = @programme.messages
+	end
+
+
+     def new
+        @programme = Programme.find(params[:programme_id])
+        		@message = @programme.messages.new
+
+    end 
+
+  def create
+        
+        @new_message = Message.new(message_params)
+        programme = Programme.find(params [:message.programme_id])
+        @new_message.programme = programme
+        @new_message.save
+        redirect_to boomerang_programme_messages_path(@new_message.programme.boomerang, @programme) 
+
+       end
+
+
+private
+def message_params
+	  params.require(:message).
+                            permit(:timelag, :message, :programme_id )
+end
+
+
+	
 end
--- rboom-app/app/models/programme.rb
+++ rboom-app/app/models/programme.rb
@@ -1,3 +1,5 @@
 class Programme < ActiveRecord::Base
   belongs_to :boomerang
+  	has_many :messages
+
 end
